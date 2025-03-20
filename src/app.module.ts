3e7f65bc080daae70448bd61fbba8adf16c2db52
gustavo hernandez <hernandez29a@gmail.com>
2025-03-20 18:47:34 -0400

    firts commit

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { join } from 'path';
+
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+      ],
+    }),
+    HelloWorldModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
