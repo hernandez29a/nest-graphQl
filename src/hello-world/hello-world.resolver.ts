aae6a28c7a19361a4a1382b6c1d15e944738a454
gustavo hernandez <hernandez29a@gmail.com>
2025-03-20 18:55:26 -0400

    generar un query ramdom

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,14 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-    @Query( () => String )
+    @Query( () => String, { description: 'Hola mundo es lo q retorna', name: 'hello'} )
     helloWorld(): string {
         return 'Hello, World!';
+    }
 
+    @Query( () => Float, { name: 'ramdomNumber', description: 'Numero aleatorio entre 0 y 1'} )
+    randomNumber(): number {
+        return Math.random() * 100;
     }
 }
